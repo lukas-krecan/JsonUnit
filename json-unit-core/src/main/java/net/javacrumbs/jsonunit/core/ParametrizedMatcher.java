c1bd4c1a2eca501d43059f8049f9993fab8fac51
Lukas Krecan <lukas@krecan.net>
2017-12-12 21:45:17 +0100

    Dependencies updated

--- json-unit-core/src/main/java/net/javacrumbs/jsonunit/core/ParametrizedMatcher.java
+++ json-unit-core/src/main/java/net/javacrumbs/jsonunit/core/ParametrizedMatcher.java
@@ -4,12 +4,12 @@ package net.javacrumbs.jsonunit.core;
  * Implement this interface to use with parametrized matchers. Sample usage
  *
  * <pre>
- *     {@code
+ *     <code>
  *
- *     Matcher<?> divisionMatcher = new DivisionMatcher();
+ *     Matcher&lt;?&gt; divisionMatcher = new DivisionMatcher();
  *     assertJsonEquals("{\"test\": \"${json-unit.matches:isDivisibleBy}3\"}", "{\"test\":5}", JsonAssert.withMatcher("isDivisibleBy", divisionMatcher));
  *
- *     private static class DivisionMatcher extends BaseMatcher<Object> implements ParametrizedMatcher {
+ *     private static class DivisionMatcher extends BaseMatcher&lt;Object&gt; implements ParametrizedMatcher {
  *       private BigDecimal param;
  *
  *       public boolean matches(Object item) {
@@ -20,7 +20,6 @@ package net.javacrumbs.jsonunit.core;
  *           description.appendValue(param);
  *       }
  *
- *       @Override
  *       public void describeMismatch(Object item, Description description) {
  *           description.appendText("It is not divisible by ").appendValue(param);
  *       }
@@ -31,7 +30,7 @@ package net.javacrumbs.jsonunit.core;
  *   }
  *
  *
- *     }
+ *   </code>
  * </pre>
  */
 public interface ParametrizedMatcher {
