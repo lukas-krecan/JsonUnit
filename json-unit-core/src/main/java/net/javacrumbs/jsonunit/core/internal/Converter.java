95045a73a9042839a821228aabec30cd741b2775
Alexander Falb <fal3xx@gmail.com>
2019-03-05 07:11:34 +0100

    integrate Johnzon/JsonP as JSON Parser

--- json-unit-core/src/main/java/net/javacrumbs/jsonunit/core/internal/Converter.java
+++ json-unit-core/src/main/java/net/javacrumbs/jsonunit/core/internal/Converter.java
@@ -41,6 +41,9 @@ class Converter {
     private static final boolean moshiPresent =
         isClassPresent("com.squareup.moshi.Moshi");
 
+    private static final boolean johnzonPresent =
+        isClassPresent("org.apache.johnzon.mapper.Mapper");
+
     Converter(List<NodeFactory> factories) {
         if (factories.isEmpty()) {
             throw new IllegalStateException("List of factories can not be empty");
@@ -62,7 +65,7 @@ class Converter {
         }
 
         if (factories.isEmpty()) {
-            throw new IllegalStateException("Please add either json.org, Jackson 1.x, Jackson 2.x or Gson to the classpath");
+            throw new IllegalStateException("Please add either json.org, Jackson 1.x, Jackson 2.x, Johnzon or Gson to the classpath");
         }
         return new Converter(factories);
     }
@@ -102,6 +105,10 @@ class Converter {
 
         if (jackson2Present) {
             factories.add(new Jackson2NodeFactory());
+        }
+
+        if (johnzonPresent) {
+            factories.add(new JohnzonNodeFactory());
         }
         return factories;
     }
