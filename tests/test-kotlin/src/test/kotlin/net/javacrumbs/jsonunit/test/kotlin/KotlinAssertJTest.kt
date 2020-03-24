7ca08c3999409000f155d40ce3759fbbc2136b6b
Lukas Krecan <lukas.krecan@adleritech.net>
2020-03-24 09:09:44 +0100

    Update Kotlin test

--- tests/test-kotlin/src/test/kotlin/net/javacrumbs/jsonunit/test/kotlin/KotlinAssertJTest.kt
+++ tests/test-kotlin/src/test/kotlin/net/javacrumbs/jsonunit/test/kotlin/KotlinAssertJTest.kt
@@ -1,6 +1,7 @@
 package net.javacrumbs.jsonunit.test.kotlin
 
 import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
+import net.javacrumbs.jsonunit.assertj.whenever
 import net.javacrumbs.jsonunit.core.ConfigurationWhen.path
 import net.javacrumbs.jsonunit.core.ConfigurationWhen.then
 import net.javacrumbs.jsonunit.core.Option
@@ -11,7 +12,7 @@ class KotlinAssertJTest {
     @Test
     fun shouldIgnoreArrayOrderInSpecificPath() {
         assertThatJson("{\"obj\":{\"a\": [1, 2], \"b\": [3, 4]}}")
-                .`when`(path("obj.a"), then(Option.IGNORING_ARRAY_ORDER))
+                .whenever(path("obj.a"), then(Option.IGNORING_ARRAY_ORDER))
                 .isEqualTo("{\"obj\":{\"a\": [2, 1], \"b\": [3, 4]}}")
     }
 }
