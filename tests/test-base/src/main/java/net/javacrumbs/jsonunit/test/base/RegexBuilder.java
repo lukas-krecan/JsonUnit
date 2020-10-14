9bb89350e9f3840e4c96566826d8c3c9dd7a7037
Lukas Krecan <lukas@krecan.net>
2020-10-14 08:32:17 +0200

    #290 Expression builder test

---
+++ tests/test-base/src/main/java/net/javacrumbs/jsonunit/test/base/RegexBuilder.java
@@ -0,0 +1,26 @@
+package net.javacrumbs.jsonunit.test.base;
+
+public class RegexBuilder {
+    private final String expression;
+
+    private RegexBuilder(String expression) {
+        this.expression = expression;
+    }
+
+    static RegexBuilder regex() {
+        return new RegexBuilder("\"${json-unit.regex}");
+    }
+
+    RegexBuilder str(String staticString) {
+        return new RegexBuilder(expression + "\\\\Q" + staticString + "\\\\E");
+    }
+
+    RegexBuilder exp(String regex) {
+        return new RegexBuilder(expression + regex);
+    }
+
+    public String toString() {
+        return expression + "\"";
+    }
+
+}
