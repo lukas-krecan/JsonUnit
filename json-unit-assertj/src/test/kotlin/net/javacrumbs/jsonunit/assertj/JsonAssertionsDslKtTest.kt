e5e4943e6b787c897665d00f9d8f27a5a38e9a84
Lukas Krecan <lukas@krecan.net>
2020-11-06 11:38:35 +0100

    Add Kotlin test

---
+++ json-unit-assertj/src/test/kotlin/net/javacrumbs/jsonunit/assertj/JsonAssertionsDslKtTest.kt
@@ -0,0 +1,12 @@
+package net.javacrumbs.jsonunit.assertj
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class JsonAssertionsDslKtTest {
+    @Test
+    fun `Function should be accessible`() {
+        assertThat(assertThatJson("1")).isNotNull()
+    }
+}
