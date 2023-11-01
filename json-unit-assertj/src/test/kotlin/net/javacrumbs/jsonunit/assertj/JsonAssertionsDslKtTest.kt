a37dde3f25a2acc9cd6e434f39347841f86c3feb
Lukas Krecan <lukas@krecan.net>
2023-11-01 21:07:47 +0100

    Spotless Kotlin

--- json-unit-assertj/src/test/kotlin/net/javacrumbs/jsonunit/assertj/JsonAssertionsDslKtTest.kt
+++ json-unit-assertj/src/test/kotlin/net/javacrumbs/jsonunit/assertj/JsonAssertionsDslKtTest.kt
@@ -3,7 +3,6 @@ package net.javacrumbs.jsonunit.assertj
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-
 class JsonAssertionsDslKtTest {
     @Test
     fun `Function should be accessible`() {
--- json-unit-kotest/src/test/kotlin/net/javacrumbs/jsonunit/kotest/test/KotestTest.kt
+++ json-unit-kotest/src/test/kotlin/net/javacrumbs/jsonunit/kotest/test/KotestTest.kt
@@ -13,6 +13,8 @@ import io.kotest.matchers.shouldNot
 import io.kotest.matchers.string.shouldBeLowerCase
 import io.kotest.matchers.throwable.haveMessage
 import io.kotest.matchers.throwable.shouldHaveMessage
+import java.math.BigDecimal.valueOf
+import kotlin.text.RegexOption.DOT_MATCHES_ALL
 import net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER
 import net.javacrumbs.jsonunit.kotest.beJsonBoolean
 import net.javacrumbs.jsonunit.kotest.beJsonNull
@@ -28,55 +30,57 @@ import net.javacrumbs.jsonunit.kotest.shouldBeJsonObject
 import net.javacrumbs.jsonunit.kotest.shouldBeJsonString
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.math.BigDecimal.valueOf
-import kotlin.text.RegexOption.DOT_MATCHES_ALL
 
 class KotestTest {
     @Test
     fun `Should assert JSON`() {
-        assertThrows<AssertionError> {
-            """{"test":1}""" should equalJson("""{"test":2}""")
-        }.shouldHaveMessage("""JSON documents are different:
-Different value found in node "test", expected: <2> but was: <1>.""")
+        assertThrows<AssertionError> { """{"test":1}""" should equalJson("""{"test":2}""") }
+            .shouldHaveMessage(
+                """JSON documents are different:
+Different value found in node "test", expected: <2> but was: <1>."""
+            )
     }
 
     @Test
     fun `Should assert JSON negate`() {
-        assertThrows<AssertionError> {
-            """{"test":1}""" shouldNot equalJson("""{"test":1}""")
-        }.shouldHaveMessage("""Expected values to not match""")
+        assertThrows<AssertionError> { """{"test":1}""" shouldNot equalJson("""{"test":1}""") }
+            .shouldHaveMessage("""Expected values to not match""")
     }
 
     @Test
     fun `Should assert JSON configuration`() {
-        """{"test":1.01}""" should equalJson("""{"test":1}""", configuration { withTolerance(0.1).withOptions(IGNORING_ARRAY_ORDER) })
+        """{"test":1.01}""" should
+            equalJson("""{"test":1}""", configuration { withTolerance(0.1).withOptions(IGNORING_ARRAY_ORDER) })
     }
 
     @Test
     fun `Should assert path`() {
-        assertThrows<AssertionError> {
-            """{"test":1}""" inPath "test" should equalJson("2")
-        }.shouldHaveMessage("""JSON documents are different:
-Different value found in node "test", expected: <2> but was: <1>.""")
+        assertThrows<AssertionError> { """{"test":1}""" inPath "test" should equalJson("2") }
+            .shouldHaveMessage(
+                """JSON documents are different:
+Different value found in node "test", expected: <2> but was: <1>."""
+            )
     }
 
     @Test
     fun `Should assert nested`() {
         assertThrows<AssertionError> {
-            """{"test": {"nested": 1}}""".inPath("test").asClue {
-                it inPath "nested" should equalJson("2")
+                """{"test": {"nested": 1}}""".inPath("test").asClue { it inPath "nested" should equalJson("2") }
             }
-        }.shouldHaveMessage("""JSON in path "test"
+            .shouldHaveMessage(
+                """JSON in path "test"
 JSON documents are different:
-Different value found in node "nested", expected: <2> but was: <1>.""")
+Different value found in node "nested", expected: <2> but was: <1>."""
+            )
     }
 
     @Test
     fun `Should assert JSON path`() {
-        assertThrows<AssertionError> {
-            """{"test":1}""" inPath ("$.test") should equalJson("""2""")
-        }.shouldHaveMessage("""JSON documents are different:
-Different value found in node "$.test", expected: <2> but was: <1>.""")
+        assertThrows<AssertionError> { """{"test":1}""" inPath ("$.test") should equalJson("""2""") }
+            .shouldHaveMessage(
+                """JSON documents are different:
+Different value found in node "$.test", expected: <2> but was: <1>."""
+            )
     }
 
     @Test
@@ -106,23 +110,20 @@ Different value found in node "$.test", expected: <2> but was: <1>.""")
 
     @Test
     fun `Should assert number fail`() {
-        assertThrows<AssertionError> {
-            """{"test": true}""" inPath "test" should beJsonNumber()
-        }.shouldHaveMessage("""Node "test" has invalid type, expected: <number> but was: <true>.""")
+        assertThrows<AssertionError> { """{"test": true}""" inPath "test" should beJsonNumber() }
+            .shouldHaveMessage("""Node "test" has invalid type, expected: <number> but was: <true>.""")
     }
 
     @Test
     fun `Should assert not number fail`() {
-        assertThrows<AssertionError> {
-            """{"test": 1}""" inPath "test" shouldNot beJsonNumber()
-        }.shouldHaveMessage("""Node "test" has invalid type, expected to not be number but was: <1>.""")
+        assertThrows<AssertionError> { """{"test": 1}""" inPath "test" shouldNot beJsonNumber() }
+            .shouldHaveMessage("""Node "test" has invalid type, expected to not be number but was: <1>.""")
     }
 
     @Test
     fun `Should assert number fail missing`() {
-        assertThrows<AssertionError> {
-            """{"test": true}""" inPath "missing" should beJsonNumber()
-        }.shouldHaveMessage("""Node "missing" is missing.""")
+        assertThrows<AssertionError> { """{"test": true}""" inPath "missing" should beJsonNumber() }
+            .shouldHaveMessage("""Node "missing" is missing.""")
     }
 
     @Test
@@ -133,17 +134,20 @@ Different value found in node "$.test", expected: <2> but was: <1>.""")
     @Test
     fun `Should assert array chained`() {
         assertThrows<AssertionError> {
-            """{"test": [1, 2, 3, 1]}""".inPath("test").shouldBeJsonArray().shouldNotContainDuplicates()
-        }.shouldHaveMessage("""Collection should not contain duplicates""")
+                """{"test": [1, 2, 3, 1]}""".inPath("test").shouldBeJsonArray().shouldNotContainDuplicates()
+            }
+            .shouldHaveMessage("""Collection should not contain duplicates""")
     }
 
     @Test
     fun `Should assert array chained forAll`() {
         assertThrows<AssertionError> {
-            """{"test": [{"a": 1}, {"a": 2}, {"a": 3}, {"a": true}]}""".inPath("test").shouldBeJsonArray().forAll {
-                it inPath "a" should beJsonNumber()
+                """{"test": [{"a": 1}, {"a": 2}, {"a": 3}, {"a": true}]}""".inPath("test").shouldBeJsonArray().forAll {
+                    it inPath "a" should beJsonNumber()
+                }
             }
-        }.shouldHaveMessage("""3 elements passed but expected 4
+            .shouldHaveMessage(
+                """3 elements passed but expected 4
 
 The following elements passed:
   [0] {"a":1}
@@ -151,54 +155,54 @@ The following elements passed:
   [2] {"a":3}
 
 The following elements failed:
-  [3] [("a", true)] => Node "a" has invalid type, expected: <number> but was: <true>.""")
+  [3] [("a", true)] => Node "a" has invalid type, expected: <number> but was: <true>."""
+            )
     }
 
     @Test
     fun `Should assert array with JSON path`() {
-        """{"test": [{"a": "a"}, {"a": true}, {"a": null}, {"a": 4}]}""".inPath("$.test[*].a")
-                .shouldBeJsonArray().shouldContainExactly("a", true, null, valueOf(4))
+        """{"test": [{"a": "a"}, {"a": true}, {"a": null}, {"a": 4}]}"""
+            .inPath("$.test[*].a")
+            .shouldBeJsonArray()
+            .shouldContainExactly("a", true, null, valueOf(4))
     }
 
     @Test
     fun `Should assert as JSON Object`() {
-        """{"a":1, "b": true}""".shouldBeJsonObject().shouldMatchAll(
-                "a" to { it should beJsonNumber() },
-                "b" to { it should beJsonBoolean() }
-        )
+        """{"a":1, "b": true}"""
+            .shouldBeJsonObject()
+            .shouldMatchAll("a" to { it should beJsonNumber() }, "b" to { it should beJsonBoolean() })
     }
 
     @Test
     fun `Should assert as JSON Object equality`() {
-        """{"a":1, "b": true, "c": null, "d": "string"}""".shouldBeJsonObject().shouldBeEqual(
-                mapOf(
-                        "a" to valueOf(1),
-                        "b" to true,
-                        "c" to null,
-                        "d" to "string"
-                )
-        )
+        """{"a":1, "b": true, "c": null, "d": "string"}"""
+            .shouldBeJsonObject()
+            .shouldBeEqual(mapOf("a" to valueOf(1), "b" to true, "c" to null, "d" to "string"))
     }
 
     @Test
     fun `Should assert as JSON Object failure`() {
         assertThrows<AssertionError> {
-            """{"a":1, "b": true}""".shouldBeJsonObject().shouldMatchAll(
-                    "a" to { it should beJsonNumber() },
-                    "b" to { it should equalJson(false) }
+                """{"a":1, "b": true}"""
+                    .shouldBeJsonObject()
+                    .shouldMatchAll("a" to { it should beJsonNumber() }, "b" to { it should equalJson(false) })
+            }
+            .shouldHaveMessage(
+                "Expected map to match all assertions. Missing keys were=[], Mismatched values were=[(b, JSON documents are different:\n" +
+                    "Different value found in node \"\", expected: <false> but was: <true>.\n" +
+                    ")], Unexpected keys were []."
             )
-        }.shouldHaveMessage("Expected map to match all assertions. Missing keys were=[], Mismatched values were=[(b, JSON documents are different:\n" +
-                "Different value found in node \"\", expected: <false> but was: <true>.\n" +
-                ")], Unexpected keys were [].")
     }
 
     @Test
     fun `Should assert softly`() {
         assertThrows<AssertionError> {
-            assertSoftly {
-                """{"a":"a", "b": true}""" inPath "a" should equalJson("b")
-                """{"a":"a", "b": true}""".inPath("a").shouldBeJsonBoolean()
+                assertSoftly {
+                    """{"a":"a", "b": true}""" inPath "a" should equalJson("b")
+                    """{"a":"a", "b": true}""".inPath("a").shouldBeJsonBoolean()
+                }
             }
-        }.should(haveMessage(Regex("The following 2 assertions failed:.*", DOT_MATCHES_ALL)))
+            .should(haveMessage(Regex("The following 2 assertions failed:.*", DOT_MATCHES_ALL)))
     }
 }
