768ecf5c74c0bf652ed45de28cb9d6651aff3fe5
Lukas Krecan <lukas@krecan.net>
2024-06-10 21:48:22 +0200

    AssertJMockMvcTest

---
+++ json-unit-spring/src/test/java/net/javacrumbs/jsonunit/spring/testit/AssertJMockMvcTest.java
@@ -0,0 +1,39 @@
+package net.javacrumbs.jsonunit.spring.testit;
+
+import static java.util.Collections.singleton;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.jsonUnitAssert;
+import static net.javacrumbs.jsonunit.spring.JsonUnitJsonComparator.comparator;
+import static net.javacrumbs.jsonunit.spring.testit.demo.ExampleController.CORRECT_JSON;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import net.javacrumbs.jsonunit.core.Configuration;
+import net.javacrumbs.jsonunit.spring.testit.demo.ExampleController;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+public class AssertJMockMvcTest {
+
+    @Test
+    void shouldWorkWithMockMvcTester() {
+        MockMvcTester mvc = MockMvcTester.of(new ExampleController());
+        assertThat(mvc.get().uri("/sample"))
+                .hasStatusOk()
+                .bodyJson()
+                // https://github.com/spring-projects/spring-framework/issues/33019
+                .isEqualTo(CORRECT_JSON, comparator(Configuration.empty().whenIgnoringPaths("result.utf8")));
+    }
+
+    @Test
+    void shouldUseConvertTo() {
+        MockMvcTester mvc = MockMvcTester.of(new ExampleController())
+                .withHttpMessageConverters(singleton(new MappingJackson2HttpMessageConverter()));
+        assertThat(mvc.get().uri("/sample"))
+                .hasStatusOk()
+                .bodyJson()
+                .convertTo(jsonUnitAssert())
+                .inPath("result.array")
+                .isArray()
+                .containsExactly(1, 2, 3);
+    }
+}
