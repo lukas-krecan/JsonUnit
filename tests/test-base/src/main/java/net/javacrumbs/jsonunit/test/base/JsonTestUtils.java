d2aa5756ec17a3ba2701cb255e56715e6d3ebade
Lukas Krecan <lukas@krecan.net>
2025-03-17 20:40:58 +0100

    Support for Jackson 3
    # Conflicts:
    #	pom.xml
    
    # Conflicts:
    #	pom.xml

--- tests/test-base/src/main/java/net/javacrumbs/jsonunit/test/base/JsonTestUtils.java
+++ tests/test-base/src/main/java/net/javacrumbs/jsonunit/test/base/JsonTestUtils.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,6 +25,10 @@ import java.util.Map;
 import org.json.JSONTokener;
 
 public class JsonTestUtils {
+
+    public static Object readByJackson3(String value) {
+        return new tools.jackson.databind.ObjectMapper().readTree(value);
+    }
 
     public static Object readByJackson2(String value) {
         try {
